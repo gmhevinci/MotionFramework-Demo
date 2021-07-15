1352f2416d7feac981886ec2848af4cacc0e3f46
何冠峰 <hevinci@hotmail.com>
2021-07-15 10:11:09 +0800

    Update demo

---
+++ RPG/Assets/GameScript/Editor/ModelMaterialProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ModelMaterialProcessor : AssetPostprocessor
+{
+	Material OnAssignMaterialModel(Material material, Renderer renderer)
+	{
+		string defaultPath = "Assets/MotionAssets/ModelDefaultMaterial.mat";
+		Material defaultMaterial = AssetDatabase.LoadAssetAtPath<Material>(defaultPath);
+		if (defaultMaterial == null)
+			throw new System.Exception("Not found model default material.");
+		return defaultMaterial;
+	}
+}
